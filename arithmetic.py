659e4f79cd93122669bedde21708a3588a672b34
Darius254 <dariuskibagendi@yahoo.com>
2024-07-05 13:21:34 +0300

    subimting my python assignment

---
+++ arithmetic.py
@@ -0,0 +1,3 @@
+x = int(input("Enter your first number: "))
+y = int(input("Enter your first number: "))
+print(f'The sum is {x + y}, ' f'division is {x/y } ' f'Substraction is {x-y} ' f'And multiplication is {x*y}')
